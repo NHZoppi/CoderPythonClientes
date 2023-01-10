89653aadff827910000ef34a0fe3060035cd0601
nicolas zoppiv <nicolaszoppi1@gmail.com>
2023-01-10 13:43:24 -0300

    Se agrego metodo str para visualizar datos

--- Clientes/models.py
+++ Clientes/models.py
@@ -7,13 +7,21 @@ class Cliente(models.Model):
     apellido = models.CharField(max_length=200)
     direccion = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f"{self.nombre}, {self.apellido}"
+
 class Articulos(models.Model):
     nombre = models.CharField(max_length=200)
     precio = models.FloatField()
     descripcion = models.TextField()
 
+    def __str__(self):
+        return f"{self.nombre}, {self.precio}"
 class Venta(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     articulo = models.ForeignKey(Articulos, on_delete=models.CASCADE)
     precio_total = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal(0.0))
     fecha = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.cliente}, {self.articulo}"
