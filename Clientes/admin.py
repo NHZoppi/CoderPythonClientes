34dd8713bd2300a6c0e4a5efad9db8d1be3fc2eb
nicolas zoppiv <nicolaszoppi1@gmail.com>
2023-01-10 13:44:51 -0300

    Nuevos models en admin.py y superuser fue creado

--- Clientes/admin.py
+++ Clientes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from Clientes.models import Venta, Articulos, Cliente
 # Register your models here.
+
+admin.site.register(Venta)
+admin.site.register(Articulos)
+admin.site.register(Cliente)
