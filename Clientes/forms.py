2bc4a73cc09cd45515da47637dd23306cfa148fd
Nicolas Zoppi <nicolaszoppi1@gmail.com>
2023-01-10 23:16:56 -0300

    Cambio login, ahora usa view auth.views. Se agrego un Logout

--- Clientes/forms.py
+++ Clientes/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django import forms
 from Clientes.models import Cliente, Articulos, Venta
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class ClienteForm(forms.ModelForm):
     class Meta:
@@ -22,3 +22,9 @@ class VentaForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['cliente'].label_from_instance = lambda obj: obj.nombre
         self.fields['articulo'].label_from_instance = lambda obj: obj.nombre
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'class':'form-control'}))
--- Clientes/urls.py
+++ Clientes/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.saludar, name='base'),
     path('cliente/', views.cliente_form_view, name='cliente_form'),
     path('articulo/', views.articulo_form_view, name='articulo_form'),
     path('venta/', views.venta_form_view, name='venta_form'),
+    path('login/', LoginView.as_view(template_name = "login.html"), name='login'),
+    path('logout/', LogoutView.as_view(template_name = "logout.html"), name='logout'),
+    path('register/', views.register_view, name='register')
 ]
 
